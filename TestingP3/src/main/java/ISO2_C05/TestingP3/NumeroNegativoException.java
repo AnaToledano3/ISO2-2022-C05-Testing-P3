4b01f41de74ff53f5be0845cfdef9aeadd52be01
AngelLuis1 <AngelLuis.Rodriguez1@alu.uclm.es>
2022-12-22 22:33:09 +0100

    Corrige problemas con los espacios y longitud de linea
    Actualiza target

--- TestingP3/src/main/java/ISO2_C05/TestingP3/NumeroNegativoException.java
+++ TestingP3/src/main/java/ISO2_C05/TestingP3/NumeroNegativoException.java
@@ -1,12 +1,8 @@
 package ISO2_C05.TestingP3;
 
-public class NumeroNegativoException extends Exception{
-
-
+public class NumeroNegativoException extends Exception {
 	public NumeroNegativoException(String message) {
 		super(message);
 	}
 
 }
-
-    
